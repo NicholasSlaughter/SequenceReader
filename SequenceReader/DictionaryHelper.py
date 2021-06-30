95141759187dd0529d9c5a579fd924484ddcc32d
NicholasSlaughter <38059177+NicholasSlaughter@users.noreply.github.com>
2021-06-29 22:43:16 -0700

    Refactored code to make it cleaner. Added commentation for most lines in the code.

--- SequenceReader/DictionaryHelper.py
+++
@@ -1,9 +0,0 @@
-
-class DictionaryHelper(object):
-    """description of class"""
-    def Add_To_Dictionary(seq,seq_dict):
-        if seq not in seq_dict:
-            seq_dict[seq]=0
-        seq_dict[seq]+=1
-        return seq_dict[seq]
-
